64eea27f57b3cb84e32d74a8edb141951289f30d
Jgwash08 <washij2@mail.uc.edu>
2026-09-24 21:04:17 -0400

    Update Main.java
    Refactor main method to include sales tax calculation.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+public class Main
+{
+    public static void main(String[] args)
+    {
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+        // Declarations
+        double purchasePrice = 0;
+        double salesTaxAmount = 0;
+        final double SALES_TAX_RATE = .05;
+
+        // User Prompt
+        System.out.println("Enter the purchase price: ");
+
+        // User Input Simulation
+        System.out.println("150");
+        purchasePrice = 150;
+
+        // Calculate the Sales Tax Amount
+        salesTaxAmount = purchasePrice * SALES_TAX_RATE;
+
+        // Output to User
+        System.out.println("The sales tax for the purchase price of $" + purchasePrice +
+                " is $" + salesTaxAmount + ".");
+    }
 }
